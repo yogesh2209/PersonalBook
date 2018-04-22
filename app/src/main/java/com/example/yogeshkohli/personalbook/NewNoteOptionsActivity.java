ef750a63c96b36e29cfa9f78f725fa1793cc2381
yogesh2209 <yogesh22kohli@gmail.com>
2018-04-22 16:52:37 -0700

    Commit - activities created only

--- app/src/main/java/com/example/yogeshkohli/personalbook/NewNoteOptionsActivity.java
+++ app/src/main/java/com/example/yogeshkohli/personalbook/NewNoteOptionsActivity.java
@@ -1,7 +1,9 @@
 package com.example.yogeshkohli.personalbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewNoteOptionsActivity extends AppCompatActivity {
 
@@ -10,4 +12,23 @@ public class NewNoteOptionsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_note_options);
     }
+
+    //Note type text Button Action
+    public void noteOptionTextButtonClicked(View button) {
+        Intent i = new Intent(this, NoteTypeTextActivity.class);
+        startActivity(i);
+    }
+
+    //Note type Draw Button Action
+    public void noteOptionDrawButtonClicked(View button) {
+        Intent i = new Intent(this, NoteTypeDrawActivity.class);
+        startActivity(i);
+    }
+
+    //Note type Audio Button Action
+    public void noteOptionAudioButtonClicked(View button) {
+        Intent i = new Intent(this, NoteTypeAudioActivity.class);
+        startActivity(i);
+    }
 }
+
---
+++ app/src/main/java/com/example/yogeshkohli/personalbook/NoteTypeTextActivity.java
@@ -0,0 +1,13 @@
+package com.example.yogeshkohli.personalbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoteTypeTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_type_text);
+    }
+}
